dbf9f554a73b28eca6aa2cc32ae1c78615873617
lsooun <lsooun@gmail.com>
2019-11-21 00:14:14 +0800

    生命周期方法

--- config/config.js
+++ config/config.js
@@ -1,12 +1,30 @@
 export default {
 	plugins:[
 		['umi-plugin-react', {
-			
+
 		}],
 	],
 	singular: true,
-	routes: [{
-    	path: '/',
-    	component: './HelloWorld',
-  	}],
+	routes: [
+		{
+	    	path: '/',
+	    	component: './HelloWorld',
+	  	},
+	  	{
+	  		path: '/shop',
+	  		component: './shoppinglist',
+	  	},
+	  	{
+	  		path: '/content',
+	  		component: './content',
+	  	},
+	  	{
+	  		path: '/square',
+	  		component: './square',
+	  	},
+	  	{
+	  		path: '/clock',
+	  		component: './clock',
+	  	}
+  	],
 };
